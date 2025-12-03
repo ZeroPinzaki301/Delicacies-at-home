3a579d546199f848280209af2620a65a77f0926a
Zeraijin301 <138296747+ZeroPinzaki301@users.noreply.github.com>
2025-12-03 16:08:51 +0800

    new update

--- frontend/src/components/DishCard.jsx
+++ frontend/src/components/DishCard.jsx
@@ -2,11 +2,26 @@ import { TbBowlFilled } from "react-icons/tb";
 
 const DishCard = ({ dishImg, dishName, width = "w-50", height = "h-50" }) => {
   return (
-    <div className={`flex flex-col items-center justify-center ${width} ${height} p-4`}>
+    <div 
+      className={`
+        flex flex-col rounded-tl-4xl 
+        rounded-bl-4xl rounded-br-4xl 
+        rounded-tr-sm bg-orange-400/25  
+        border-2 border-amber-900/50
+        items-center justify-center ${width} ${height} px-3 py-1`}>
       
-      
+      <div 
+        className="
+          cursor-pointer
+          rounded-b-[4em] md:rounded-b-[6.5em] 
+          w-[90%] h-[55%] md:h-[60%] hover:w-[95%] 
+          hover:h-[60%] md:hover:h-[65%] bg-cover bg-center
+          ease-out duration-300" 
+        style={{ backgroundImage: `url(${dishImg})`}}>
+
+      </div>
     
-      <h3 className="mt-3 text-center font-medium text-gray-800 truncate w-full">
+      <h3 className="mt-1 text-center text-xl font-bold tracking-widest text-amber-900 w-full">
         {dishName}
       </h3>
     </div>
--- frontend/src/pages/Dishes.jsx
+++ frontend/src/pages/Dishes.jsx
@@ -1,17 +1,90 @@
 import DishCard from "../components/DishCard"
 
-
-
 const Dishes = () => {
-  return (
-    <div className="min-h-[90vh]">
-      <div className="mt-">
-        <DishCard 
-          dishName="TestName"
-          dishImg="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbwDAkKWDA1mGQafmeIFS9rWlEw0pTxlGO7A&s"
-        />
-      </div>
 
+  const cloudinaryLink = 'https://res.cloudinary.com/dqs9oiho2/image/upload/';
+
+  const WeekFav1 = `${cloudinaryLink}v1763443535/1stDishImg_nnddos.png`;
+  const WeekFav2 = `${cloudinaryLink}v1763443542/2ndDishImg_vmk4bc.png`;
+  const WeekFav3 = `${cloudinaryLink}v1763443512/3rdDishImg_lugyg1.png`;
+  const WeekFav4 = `${cloudinaryLink}v1763443541/4thDishImg_fluehj.png`;
+  const WeekFav5 = `${cloudinaryLink}v1763443518/5thDishImg_l18zg6.png`;
+  const WeekFav6 = `${cloudinaryLink}v1763443541/6thDishImg_mt8ncl.png`;
+  const WeekFav7 = `${cloudinaryLink}v1763443523/7thDishImg_lnhckt.png`;
+  const WeekFav8 = `${cloudinaryLink}v1763443518/8thDishImg_wnnfm2.png`;
+
+  return (
+    <div className="min-h-screen px-1 md:px-10 py-10 bg-yellow-100 overflow-x-hidden">
+
+      <h1 className="mt-12 text-center text-6xl tracking-widest">Category of Dishes</h1>
+
+      <div className="mt-5 gap-1 md:gap-7 md:px-15 grid grid-cols-2 md:grid-cols-4">
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Pork"
+            dishImg={WeekFav2}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Chicken"
+            dishImg={WeekFav8}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Fish"
+            dishImg={WeekFav6}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Vegetable"
+            dishImg={WeekFav7}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Noodle"
+            dishImg={WeekFav4}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Beef"
+            dishImg={WeekFav3}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Seafood"
+            dishImg={WeekFav1}
+          />
+        </div>
+        <div className="mt-">
+          <DishCard 
+            width="w-full"
+            height="md:h-[16.15em] h-[11em]"
+            dishName="Dessert"
+            dishImg={WeekFav5}
+          />
+        </div>
+      </div>
+    
     </div>
   )
 }
